e45a5316e63f5a034cb3932d4de7396b1a6d84d1
Robert Bost <bostrt@users.noreply.github.com>
2017-06-07 02:48:29 -0400

    Support for single integer size values for square thumbnails (#9)
    * Support for single integer size values for square thumbnails
    
    * Accept numeric size parameter
    
    * Accept numeric size parameter in python 3 where no long type exists
    
    * Support tuple sizes. Ensure string value with format like "50x50" is passed into generate_filename. Expanded tests.
    
    * Fixed py2-to-3 compatibility issue.
    
    * Improved size parse error handling.

--- flask_thumbnails/utils.py
+++ flask_thumbnails/utils.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals
 
 import os
+import sys
 import importlib
 
 
@@ -26,3 +27,41 @@ def generate_filename(original_filename, *options):
     name += ext
 
     return name
+
+def parse_size(size):
+    if sys.version_info < (3,):
+        integer_types = (int, long)
+    else:
+        integer_types = (int,)
+
+    if isinstance(size, integer_types):
+        # If the size parameter is a single number, assume square aspect.
+        return [size, size]
+
+    if isinstance(size, (tuple, list)):
+        if len(size) == 1:
+            # If single value tuple/list is provided, exand it to two elements
+            return size + type(size)(size)
+        return size
+
+    try:
+        thumbnail_size = [int(x) for x in size.lower().split('x', 1)]
+    except ValueError:
+        raise ValueError('Bad size format. Valid format is **x**.')
+
+    if len(thumbnail_size) == 1:
+        # If the size parameter only contains a single integer, assume square aspect.
+        thumbnail_size.append(thumbnail_size[0])
+
+    return thumbnail_size
+
+def aspect_to_string(size):
+    if sys.version_info < (3,):
+        str_type = basestring
+    else:
+        str_type = str
+
+    if isinstance(size, str_type):
+        return size
+
+    return 'x'.join(map(str, size))
