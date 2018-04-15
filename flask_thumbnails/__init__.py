513a0b697a54d684116a1812ebeb9cfc98e917de
silentsokolov <silentsokolov@gmail.com>
2018-04-15 15:43:00 +0300

    Prepare 1.0.4

--- flask_thumbnails/__init__.py
+++ flask_thumbnails/__init__.py
@@ -14,7 +14,7 @@ except ImportError:
 
 from .utils import import_from_string, generate_filename, parse_size, aspect_to_string
 
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 
 
 class Thumbnail(object):
--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ import re
 from setuptools import setup
 
 
+def read_md(f):
+    return open(f, 'r').read()
+
+
 def get_version(package):
     init_py = open(os.path.join(package, '__init__.py')).read()
     return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
@@ -21,23 +25,31 @@ setup(
     version=get_version('flask_thumbnails'),
     url='https://github.com/silentsokolov/flask-thumbnails',
     license='MIT',
+    description='A simple extension to create a thumbs for the Flask',
+    long_description=read_md('README.rst'),
     author='Dmitriy Sokolov',
     author_email='silentsokolov@gmail.com',
-    description='A simple extension to create a thumbs for the Flask',
-    zip_safe=False,
+    packages=get_packages('flask_thumbnails'),
     include_package_data=True,
+    install_requires=[],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    zip_safe=False,
     platforms='any',
     test_suite='tests',
-    packages=get_packages('flask_thumbnails'),
-    install_requires=[],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
+        'Framework :: Flask',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities',
     ]
 )
